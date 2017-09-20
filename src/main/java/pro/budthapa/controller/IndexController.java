a7998aee55e6794eb9f992eef40ba0343c8267b0
Buddhi Bal Thapa <budthapa@gmail.com>
2017-09-20 14:06:40 +0400

    added index page

---
+++ src/main/java/pro/budthapa/controller/IndexController.java
@@ -0,0 +1,13 @@
+package pro.budthapa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+	
+	@GetMapping({"","/","/index"})
+	public String getIndexPage() {
+		return "index";
+	}
+}
